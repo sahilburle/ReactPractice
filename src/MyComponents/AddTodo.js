7d040ed4281c2713939889ad42f16dbb3be95d77
sahilburle <sahilburle1998@gmail.com>
2022-08-08 12:06:47 +0530

    react

--- src/MyComponents/AddTodo.js
+++ src/MyComponents/AddTodo.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTodo = (props) => {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const submit = ()=>{
+
+  }
+  
   return (
     <div className="container my-3">
+
       <h3>Add a Todo</h3>
       <form onSubmit={submit}>
+
         <div className="mb-3">
           <label for="title" className="form-label"> ToDo Title </label>
-          <input type="text" className="form-label" id="title" aria-describedby="emailHelp"/>
+          <input type="text" value={title} className="form-label" id="title" aria-describedby="emailHelp"/>
         </div>
+
         <div className="mb-3">
           <label for="desc" className="form-label">Todo Description</label>
-          <input type="text" className="form-control" id="desc"/>
+          <input type="text" value={desc} className="form-control" id="desc"/>
         </div>
 
         <button type="submit" className="btn btn-primary">
